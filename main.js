1b787d42105afb1e4001a9ca491e88a46ca6b8f1
Vedant-Pujari <vedantpujari15@gmail.com>
2025-09-09 08:24:59 +0530

    Fixed

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 function loadPage(page){
     document.getElementById("cf").src = page;
-=======
-function loadPage(page){
-    document.getElementById("cf").src = page;
->>>>>>> f768050d56d50875869493bd9effe7c95f35b338
 }
