877ac16251764c9f802861c5ae8c5d1634c88524
Vedant-Pujari <vedantpujari15@gmail.com>
2025-09-14 17:54:24 +0530

    Added a new section and updated rest.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function loadPage(page){
     document.getElementById("cf").src = page;
 }
+
+function loadWebsite(web){
+    if(web=="github"){
+        window.open("https://github.com/Vedant-Pujari")
+    }
+    if(web=="linkedin"){
+        window.open("https://linkedin.com/in/vedant-pujari-2607a6380")
+    }
+}
